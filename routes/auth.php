f9d04af783f1e1dc8e6ea9e00d911f811365e064
hichamcc <gaddariowen@gmail.com>
2026-02-16 10:46:04 +0000

    update routes

--- routes/auth.php
+++ routes/auth.php
@@ -9,8 +9,8 @@ use App\Http\Controllers\Auth\VerificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegistrationController::class, 'create'])->name('register');
-    Route::post('register', [RegistrationController::class, 'store']);
+    //Route::get('register', [RegistrationController::class, 'create'])->name('register');
+    //Route::post('register', [RegistrationController::class, 'store']);
 
     Route::get('login', [LoginController::class, 'create'])->name('login');
     Route::post('login', [LoginController::class, 'store']);
